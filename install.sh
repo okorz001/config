fc59dec55d9d0920efaf0b9bb2db91f4292ef35f
Oscar Korz <okorz001@gmail.com>
2012-05-03 23:51:54 -0700

    add install script
    Just a simple bash script to create symbolic links for all tracked
    configuration files.

---
+++ install.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+# Set up symbolic links for configuration files.
+
+error () {
+    echo "$(basename $0): error: $1" >&2
+}
+
+while getopts ":hb:fnq" opt; do
+    case "$opt" in
+        h)
+            cat <<HERE
+Usage: $(basename $0) [-hfnq] [-b SUFFIX]
+
+Set up symbolic links for configuration files.
+
+Options:
+    -h              Show this help message and quit.
+    -b SUFFIX       Backup existing files with the specified suffix.
+    -f              Overwrite existing files.
+    -n              Do not execute any commands (i.e. dry run).
+    -q              Do not echo commands before executing.
+HERE
+            exit 0
+            ;;
+        b)
+            suffix="$OPTARG"
+            ;;
+        f)
+            force="-f"
+            ;;
+        n)
+            dry_run=1
+            ;;
+        q)
+            quiet=1
+            ;;
+        \?)
+            error "invalid option: -$OPTARG"
+            exit 1
+            ;;
+        :)
+            error "missing argument for option: -$OPTARG"
+            exit 1
+            ;;
+    esac
+done
+
+# List of files to install. Every two elements are logically paired; the first
+# is the repository relative path and the second is the absolute install path.
+files=(
+    "bashrc"        "$HOME/.bashrc"
+    "colordiffrc"   "$HOME/.colordiffrc"
+    "conkyrc"       "$HOME/.conkyrc"
+    "gdbinit"       "$HOME/.gdbinit"
+    "gitconfig"     "$HOME/.gitconfig"
+    "gitignore"     "$HOME/.gitignore"
+    "hgrc"          "$HOME/.hgrc"
+    "inputrc"       "$HOME/.inputrc"
+    "profile"       "$HOME/.profile"
+    "screenrc"      "$HOME/.screenrc"
+    "vimrc"         "$HOME/.vimrc"
+)
+
+# Execute a command while respecting the -n and -q options.
+# TODO: This breaks if shell control characters are used. (e.g. a pipe)
+execute () {
+    [[ -z "$quiet" ]] && echo "$@"
+    [[ -z "$dry_run" ]] && $@
+}
+
+for (( i = 0; i < ${#files[@]}; i += 2 )); do
+    src="${files[i]}"
+    dst="${files[i + 1]}"
+
+    if [[ -f "$dst" && -n "$suffix" ]]; then
+        execute cp "$dst" "$dst$suffix"
+    fi
+
+    execute ln -s "$force" "$src" "$dst"
+done
