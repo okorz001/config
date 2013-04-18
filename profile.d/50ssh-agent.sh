315f2338823cf34b96b551181dcfbfe70d4bc97c
Oscar Korz <okorz001@gmail.com>
2013-04-18 12:31:50 -0700

    profile: temporarily suppress ssh-agent
    I need to integrate this better with LXDE; LXDE is spawning
    gnome-keyring on login.

--- profile.d/50ssh-agent.sh
+++ profile.d/50ssh-agent.sh
@@ -1,10 +1,10 @@
-# Define the ssh-agent socket name so that we can find instances more easily.
-SSH_AUTH_SOCK="/tmp/ssh-$USER"
-
-if [ -S "$SSH_AUTH_SOCK" ]; then
-    # Use the existing agent.
-    export SSH_AUTH_SOCK
-else
-    # Create a new agent and use our special socket name.
-    eval `ssh-agent -sa "$SSH_AUTH_SOCK"` >/dev/null
-fi
+## Define the ssh-agent socket name so that we can find instances more easily.
+#SSH_AUTH_SOCK="/tmp/ssh-$USER"
+#
+#if [ -S "$SSH_AUTH_SOCK" ]; then
+#    # Use the existing agent.
+#    export SSH_AUTH_SOCK
+#else
+#    # Create a new agent and use our special socket name.
+#    eval `ssh-agent -sa "$SSH_AUTH_SOCK"` >/dev/null
+#fi
